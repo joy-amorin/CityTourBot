2fcdb61bee6b3c863383ff55b01a697dd66330b1
joy-amorin <amorinkaren2021@gmail.com>
2024-07-03 15:41:33 -0300

    basic frontend configuration

---
+++ frontend/js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('query-form')
+    const queryInput = document.getElementById('query-input')
+    const responseText = document.getElementById('response-text')
+
+    form.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const query = queryInput.value;
+
+        try{
+            const response = await fetch('http://127.0.0.1:8000/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ query }),
+            });
+            const data = await response.json();
+            responseText.textContent = data.response;
+        }catch (error) {
+            responseText.textContent = 'Error al obtener la repuesta'
+        }
+    });
+});
