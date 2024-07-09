e55729aafbb1d4b59ce2ed83a374e1e73cd92e2c
joy-amorin <amorinkaren2021@gmail.com>
2024-07-09 17:41:33 -0300

    basic frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const query = queryInput.value;
 
         try{
-            const response = await fetch('http://127.0.0.1:8000/query', {
+            const response = await fetch('http://127.0.0.1:8000/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
