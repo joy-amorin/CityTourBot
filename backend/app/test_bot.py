4a811739d7f2afa8b3c03fec102b86ee79748ce6
joy-amorin <amorinkaren2021@gmail.com>
2024-07-11 23:56:52 -0300

    script to test the chatbot

--- backend/app/test_bot.py
+++ backend/app/test_bot.py
@@ -1,7 +1,8 @@
+import json
 import requests
 
-# URL de tu servidor FastAPI
-url = "http://127.0.0.1:8000/chat"
+# URL de tu servidor FastAPI donde está alojado el chatbot
+url = "http://127.0.0.1:8000/chat"  # Actualiza con tu propia URL
 
 # Función para interactuar con el chatbot
 def interact_with_chatbot():
@@ -17,7 +18,23 @@ def interact_with_chatbot():
 
         # Enviar la solicitud al chatbot
         response = send_message_to_chatbot(user_input)
-        print(f"Chatbot: {response['response']}\n")
+
+        if 'response' in response:
+            print(f"Chatbot: {response['response']}\n")
+        elif 'message' in response:
+            print(f"Chatbot: {response['message']}\n")
+            if 'events' in response:
+                print("Eventos Disponibles:")
+                for event in response['events']:
+                    print(f"- Nombre: {event['name']}")
+                    print(f"  Descripción: {event['description']}")
+                    print(f"  Inicia: {event['start']} - Termina: {event['end']}")
+                    print(f"  URL: {event['url']}")
+                    print()
+                print(response.get('query_next_month', ''))
+                print(response.get('exit_message', ''))
+        else:
+            print(f"Chatbot: Respuesta no reconocida del servidor.\n")
 
 def send_message_to_chatbot(message):
     headers = {
