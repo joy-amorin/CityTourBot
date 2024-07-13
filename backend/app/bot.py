eecbcaea9f4b665a5c1d8bbbab5e407618416b0f
joy-amorin <amorinkaren2021@gmail.com>
2024-07-13 18:57:52 -0300

    add handle_unrecognized_month function

--- backend/app/bot.py
+++ backend/app/bot.py
@@ -3,6 +3,10 @@ import json
 # Global variable for storing the status of the conversation
 conversation_state = {"last_question": None}
 
+def handle_unrecognized_month(user_query):
+    conversation_state["last_question"] = "evento_presencial_mes"
+    return {"response": f"Lo siento, no entendí el mes que quieres consultar en '{user_query}', inténtalo de nuevo.\nPuedes escribir 'exit' para volver al inicio."}
+
 def chat(query):
     global conversation_state
     user_query = query.lower()
@@ -29,14 +33,14 @@ def chat(query):
             conversation_state["last_question"] = "preguntar_evento_presencial"
             if events:
                 response = {
-                    "message": "Aquí están los eventos online disponibles. Si deseas más detalles, puedes entrar al link.",
+                    "message": "Aquí están los eventos online disponibles. Si deseas más detalles, puedes entrar al link!!!",
                     "events": events,
                     "query_next_month": "¿Te gustaría ver eventos presenciales también? Responde 'sí' o 'no'.",
                     "exit_message": "Puedes escribir 'exit' para volver al inicio."
                 }
                 return response
             else:
-                return {"response": "No hay eventos online disponibles por el momento, pero puedes intentar luego y ver si hay novedades.\n¿Te gustaría consultar eventos presenciales también? Responde con 'sí' o 'no'.\nPuedes escribir 'exit' para volver al inicio."}
+                return {"response": "No hay eventos online disponibles por el momento, pero puedes intentar luego y ver si hay novedades!!\n¿Te gustaría consultar eventos presenciales también? Responde con 'sí' o 'no'.\nPuedes escribir 'exit' para volver al inicio."}
         else:
             # If the answer is not valid, repeat the question
             return {"response": "¿Qué tipo de eventos te gustaría conocer?\n1. Eventos presenciales\n2. Eventos online"}
@@ -53,13 +57,16 @@ def chat(query):
                     "query_next_month": "¿Te gustaría ver eventos de otro mes? Responde 'sí' o 'no'.",
                     "exit_message": "Puedes escribir 'exit' para volver al inicio."
                 }
+                conversation_state["last_question"] = "preguntar_otro_mes"
                 return response
             else:
                 response = {
-                    "response": "No hay eventos disponibles para este mes por el momento, pero puedes intentar luego y ver si hay novedades. ¿Te gustaría ver eventos de otro mes? Responde con 'sí' o 'no'. Puedes escribir 'exit' para volver al inicio."
+                    "message": "No hay eventos disponibles para el mes actual, pero puedes intentar luego y ver si hay novedades!!",
+                    "query_next_month": "¿Te gustaría ver eventos de otro mes? Responde con 'sí' o 'no'.",
+                    "exit_message": "Puedes escribir 'exit' para volver al inicio."
                 }
-            conversation_state["last_question"] = "preguntar_otro_mes"
-            return response
+                conversation_state["last_question"] = "preguntar_otro_mes"
+                return response
 
         # Specific month query
         elif any(month in user_query for month in ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", "noviembre", "diciembre"]):
@@ -69,42 +76,40 @@ def chat(query):
                 events = get_events_by_specific_month(month)
                 if events:
                     response = {
-                        "message": "Claro, te mostraré los eventos:",
-                        "month": month,
+                        "message": f"Claro, te mostraré los eventos de interés del mes {month}:",
                         "events": events,
-                        "query_next_month_and_exit": "¿Te gustaría ver eventos de otro mes? Puedes escribir 'exit' para volver al inicio."
+                        "query_next_month": "¿Te gustaría ver eventos de otro mes? Responde 'sí' o 'no'.",
+                        "exit_message": "Puedes escribir 'exit' para volver al inicio."
                     }
+                    conversation_state["last_question"] = "preguntar_otro_mes"
+                    return response
                 else:
                     response = {
-                        "message": "No hay eventos disponibles para el mes",
-                        "month": month, 
-                        "text":" pero puedes intentar luego y ver si hay novedades!!!.",
-                        "query_next_month_and_exit": "¿Te gustaría ver eventos de otro mes? Responde con 'sí' o 'no'. Puedes escribir 'exit' para volver al inicio."
+                        "message": f"No hay eventos disponibles para el mes {month}, pero puedes intentar luego y ver si hay novedades!!!",
+                        "query_next_month": "¿Te gustaría ver eventos de otro mes? Responde con 'sí' o 'no'.",
+                        "exit_message": "Puedes escribir 'exit' para volver al inicio."
                     }
-                conversation_state["last_question"] = "preguntar_otro_mes"
-                return response
-            return {"response": "No entendí qué mes quieres consultar. ¿Podrías ser más específico?\nPuedes escribir 'exit' para volver al inicio."}
-
+                    conversation_state["last_question"] = "preguntar_otro_mes"
+                    return response
+            else:
+                return handle_unrecognized_month(user_query)
         else:
             # If the answer is not valid, repeat the question
-            return {"response": "¿Quieres ver los eventos locales del mes actual o de algún otro mes?\nPuedes escribir 'exit' para volver al inicio."}
-
+            return handle_unrecognized_month(user_query)
     # Handling of asking for another month
     if conversation_state["last_question"] == "preguntar_otro_mes":
         if "si" in user_query or "sí" in user_query:
             conversation_state["last_question"] = "evento_presencial_mes"
             return {"response": "¿Qué mes te gustaría consultar?\nPuedes escribir 'exit' para volver al inicio."}
-
         elif "no" in user_query:
             conversation_state["last_question"] = "tipo_evento"
             return {"response": "¡Entendido! ¿Qué tipo de eventos te gustaría conocer?\n1. Eventos presenciales\n2. Eventos online\nPuedes escribir 'exit' para volver al inicio."}
-
         else:
             return {"response": "¿Te gustaría ver eventos de otro mes? Responde con 'sí' o 'no'.\nPuedes escribir 'exit' en cualquier momento para volver al inicio."}
 
     # Handling of asking about on-site events after online events
     if conversation_state["last_question"] == "preguntar_evento_presencial":
-        if "si" in user_query or "si" in user_query:
+        if "si" in user_query or "sí" in user_query:
             conversation_state["last_question"] = "evento_presencial_mes"
             return {"response": "¿Quieres ver los eventos locales del mes actual o de algún otro mes?\nPuedes escribir 'exit' en cualquier momento para volver al inicio."}
         elif "no" in user_query:
